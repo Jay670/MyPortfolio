abb09ace7a8b4629a5ebcac6558b900d02d78dd3
Jay670 <32456694+Jay670@users.noreply.github.com>
2019-11-18 01:34:53 +0530

    committing

---
+++ build/precache-manifest.59baebe7b482fa1abc4f00eb9f234266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df1f5426b66c887a6536b8a137717a3",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "5da5471000f17583be31",
+    "url": "/myportfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d3972d740e0a05e32259",
+    "url": "/myportfolio/static/js/2.a7ef5556.chunk.js"
+  },
+  {
+    "revision": "5da5471000f17583be31",
+    "url": "/myportfolio/static/js/main.034431d9.chunk.js"
+  },
+  {
+    "revision": "81a0c1476aece9834d01",
+    "url": "/myportfolio/static/js/runtime-main.f5317838.js"
+  }
+]);
